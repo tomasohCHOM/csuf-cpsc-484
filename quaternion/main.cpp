2b61ebc1cc94029362440e9d42a70cd146f7e4fa
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-02-28 19:59:06 -0800

    start quaternion hw

---
+++ quaternion/main.cpp
@@ -0,0 +1,18 @@
+//============================================================
+// file: main.cpp
+//============================================================
+#include <cassert>
+#include <cstring>
+
+// MATRIX and VECTOR classes assignment
+#include "matrix3d.h"
+#include "vector3d.h"
+#include "quaternion.h"
+
+int main(int argc, const char *argv[]) {
+  vector3D::run_tests();
+  matrix3D::run_tests();
+  quaternionD::run_tests();
+
+  return 0;
+}
